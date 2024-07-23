29ee579c8e2b2fe096697f298bbb625acdf96891
theNarshofficial <thenareshoffcial@gmail.com>
2024-07-23 11:31:28 +0530

    Initial Ansi Code Updated

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 from ansi import *
 
@@ -12,6 +14,9 @@ def text_style():
     print(f"{BLINK}This is BLINK")
     print(f"{REVERSE}This is REVERSE")
     print(f"{HIDDEN}This is HIDDEN")
+    print(f"{STRIKETHROUGH}This is STRIKETHROUGH")
+    print(f"{DOUBLE_UNDERLINE}This is DOUBLE_UNDERLINE")
+    print(f"{NORMAL_INTENSITY}This is NORMAL_INTENSITY")
 
 
 def foreground_color():
@@ -24,6 +29,13 @@ def foreground_color():
     print(f"{YELLOW}This is YELLOW")
     print(f"{CYAN}This is CYAN")
     print(f"{WHITE}This is WHITE")
+    print(f"{BRIGHT_BLACK}This is BRIGHT_BLACK")
+    print(f"{BRIGHT_RED}This is BRIGHT_RED")
+    print(f"{BRIGHT_YELLOW}This is BRIGHT_YELLOW")
+    print(f"{BRIGHT_BLUE}This is BRIGHT_BLUE")
+    print(f"{BRIGHT_MAGENTA}This is BRIGHT_MAGENTA")
+    print(f"{BRIGHT_CYAN}This is BRIGHT_CYAN")
+    print(f"{BRIGHT_WHITE}This is BRIGHT_WHITE")
 
 
 def bg_color():
@@ -36,6 +48,13 @@ def bg_color():
     print(f"{BG_YELLOW}This is BG_YELLOW")
     print(f"{BG_BLUE}This is BG_BLUE")
     print(f"{BG_WHITE}This is BG_WHITE")
+    print(f"{BG_BRIGHT_RED}This is BG_BRIGHT_RED")
+    print(f"{BG_BRIGHT_BLACK}This is BG_BRIGHT_BLACK")
+    print(f"{BG_BRIGHT_GREEN}This is BG_BRIGHT_GREEN")
+    print(f"{BG_BRIGHT_YELLOW}This is BG_BRIGHT_YELLOW")
+    print(f"{BG_BRIGHT_BLUE}This is BG_BRIGHT_BLUE")
+    print(f"{BG_BRIGHT_MAGENTA}This is BG_BRIGHT_MAGENTA")
+    print(f"{BG_BRIGHT_WHITE}This is BG_BRIGHT_WHITE")
 
 
 name = os.name
@@ -69,6 +88,6 @@ while True:
     except ValueError:
         print("oops Invalid input!!!")
     except KeyboardInterrupt:
-        print(f"{RED}oops Invalid input!!!")
+        pass
     except EOFError:
         print(f"{RED}oops Invalid input!!!")
